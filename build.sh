8179c697b1b143a09601b06135d67b0d651d1c22
willguo <willguo@tencent.com>
2016-12-29 15:58:21 +0800

    调整gradle 2.14.1

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 export ANDROID_HOME=$ANDROID_SDK
 export JAVA_HOME=$JDK8
 export PATH=$JDK7/bin:$GRADLE_HOME/bin:$PATH
-export GRADLE_HOME=/data/rdm/apps/gradle/gradle-2.2.1
+export GRADLE_HOME=/data/rdm/apps/gradle/gradle-2.14.1
 export PATH=$JDK7/bin:$GRADLE_HOME/bin:$PATH
 PROJECT_PATH=iLiveSDK_Demos/
 PACKAGE_NAME=iLiveSDK_Demos_${MajorVersion}.${MinorVersion}.${FixVersion}
