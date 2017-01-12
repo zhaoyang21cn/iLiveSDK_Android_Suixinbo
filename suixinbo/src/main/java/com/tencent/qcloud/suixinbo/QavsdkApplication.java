3e5c8df4f21c471489b434782fa38f963ce712e6
xkazer <xkazer@sina.com>
2017-01-12 16:31:39 +0800

    优化部分流程

--- suixinbo/src/main/java/com/tencent/qcloud/suixinbo/QavsdkApplication.java
+++ suixinbo/src/main/java/com/tencent/qcloud/suixinbo/QavsdkApplication.java
@@ -1,10 +1,13 @@
 package com.tencent.qcloud.suixinbo;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
 
 import com.tencent.qcloud.suixinbo.presenters.InitBusinessHelper;
 import com.tencent.qcloud.suixinbo.utils.SxbLogImpl;
+
+import java.util.List;
 
 
 /**
@@ -21,15 +24,31 @@ public class QavsdkApplication extends Application {
         app = this;
         context = getApplicationContext();
 
-        SxbLogImpl.init(getApplicationContext());
+        if (shouldInit()) {
+            SxbLogImpl.init(getApplicationContext());
 
-        //初始化APP
-        InitBusinessHelper.initApp(context);
+            //初始化APP
+            InitBusinessHelper.initApp(context);
+        }
 
 
 //        LeakCanary.install(this);
 
         //创建AVSDK 控制器类
+    }
+
+    private boolean shouldInit() {
+        ActivityManager am = ((ActivityManager) getSystemService(Context.ACTIVITY_SERVICE));
+        List<ActivityManager.RunningAppProcessInfo> processInfos = am.getRunningAppProcesses();
+        String mainProcessName = getPackageName();
+        int myPid = android.os.Process.myPid();
+
+        for (ActivityManager.RunningAppProcessInfo info : processInfos) {
+            if (info.pid == myPid && mainProcessName.equals(info.processName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Context getContext() {
