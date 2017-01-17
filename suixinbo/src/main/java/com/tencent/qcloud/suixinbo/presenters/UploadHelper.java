6796cecb815ba9a33da14dc0a03f0d16c3579dac
xkazer <xkazer@sina.com>
2017-01-17 10:24:25 +0800

    修复小米5机器无法上传封面问题

--- suixinbo/src/main/java/com/tencent/qcloud/suixinbo/presenters/UploadHelper.java
+++ suixinbo/src/main/java/com/tencent/qcloud/suixinbo/presenters/UploadHelper.java
@@ -17,6 +17,11 @@ import com.tencent.upload.task.IUploadTaskListener;
 import com.tencent.upload.task.data.FileInfo;
 import com.tencent.upload.task.impl.FileUploadTask;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 /**
  * Cos人图片上传类
  */
@@ -93,6 +98,38 @@ public class UploadHelper extends Presenter {
 //        SxbLog.d(TAG, "doUpdateSig->get sig: " + sig);
     }
 
+    /**
+     * 复制单个文件
+     * @param oldPath String 原文件路径 如：c:/fqf.txt
+     * @param newPath String 复制后路径 如：f:/fqf.txt
+     * @return boolean
+     */
+    public boolean copyFile(String oldPath, String newPath) {
+        try {
+            int bytesum = 0;
+            int byteread = 0;
+            File oldfile = new File(oldPath);
+            if (oldfile.exists()) { //文件存在时
+                InputStream inStream = new FileInputStream(oldPath); //读入原文件
+                FileOutputStream fs = new FileOutputStream(newPath);
+                byte[] buffer = new byte[1444];
+                int length;
+                while ( (byteread = inStream.read(buffer)) != -1) {
+                    bytesum += byteread; //字节数 文件大小
+                    System.out.println(bytesum);
+                    fs.write(buffer, 0, byteread);
+                }
+                inStream.close();
+            }
+        }
+        catch (Exception e) {
+            SxbLog.e(TAG, "copy file failed!");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
     private void doUploadCover(final String path, boolean bRetry) {
         String sig = MySelfInfo.getInstance().getCosSig();
         if (TextUtils.isEmpty(sig)) {
@@ -106,10 +143,16 @@ public class UploadHelper extends Presenter {
             return;
         }
 
+        String tmpPath = path;
+        if ("Xiaomi".equals(android.os.Build.MANUFACTURER)) { // 复制到tmp文件再上传(小米5机器上无法占用文件)
+            tmpPath = path + "_tmp";
+            copyFile(path, tmpPath);
+        }
+
         UploadManager fileUploadMgr = new UploadManager(mContext, appid,
                 Const.FileType.File, "qcloudphoto");
-        SxbLog.d(TAG, "upload cover: " + path);
-        FileUploadTask task = new FileUploadTask(bucket, path, createNetUrl(), null, new IUploadTaskListener() {
+        SxbLog.d(TAG, "upload cover: " + tmpPath);
+        FileUploadTask task = new FileUploadTask(bucket, tmpPath, createNetUrl(), null, new IUploadTaskListener() {
             @Override
             public void onUploadSucceed(final FileInfo result) {
                 SxbLog.i(TAG, "upload succeed: " + result.url);
